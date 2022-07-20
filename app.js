3bcd4601675252fd6a38cdcbd4ecc9e59084b777
Ivan Culebra <ivan.culebra.munera@gmail.com>
2022-07-20 22:17:43 +0200

    some styles

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
